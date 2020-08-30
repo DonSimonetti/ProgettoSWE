58948b07be3af878fac97adc3e97487aebca0c77
matteo <matt.simonetti@hotmail.com>
2020-08-30 12:51:09 +0200

    Added TimeRecord class

--- SWE_BookShop/src/main/java/TimeRecord.java
+++ SWE_BookShop/src/main/java/TimeRecord.java
@@ -1,9 +1,31 @@
 package main.java;
 
+import java.sql.Time;
+
 public class TimeRecord
 {
-    public TimeRecord()
-    {
+    private Time startTime;
+    private Time endTime;
 
+    public TimeRecord(Time startTime, Time endTime)
+    {
+        this.startTime=startTime;
+        this.endTime=endTime;
+    }
+
+    public Time getStartTime()
+    {
+        return startTime;
+    }
+
+    public Time getEndTime()
+    {
+        return endTime;
+        //or return new Time(endTime.getTime()); ??
+    }
+
+    public long getTimeSpan()
+    {
+        return endTime.getTime()-startTime.getTime();
     }
 }
