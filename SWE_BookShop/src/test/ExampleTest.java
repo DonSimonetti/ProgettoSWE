47781743f829c53542c22930108fd0e8dc448dc0
matteo <matt.simonetti@hotmail.com>
2020-08-30 15:41:24 +0200

    Added ExampleTest

---
+++ SWE_BookShop/src/test/ExampleTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest
+{
+    @Test
+    void bTest()
+    {
+        int iTest=5;
+        Assertions.assertEquals(iTest,5);
+    }
+
+    @Test
+    void iTest()
+    {
+        int iTest=5;
+        Assertions.assertEquals(iTest,5);
+    }
+}
